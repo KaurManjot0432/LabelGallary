d76c240681e8b06b7273cd5f80ac880ad04601de
KaurManjot0432 <0432manjot@gmail.com>
2023-11-18 21:27:38 +0530

    changed user backend name

--- server/accounts/apps.py
+++ server/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RestapiConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restapi'
+    name = 'accounts'
